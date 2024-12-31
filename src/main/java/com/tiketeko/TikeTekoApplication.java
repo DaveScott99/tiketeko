8f54f8d1eaba947349d12090da21f54aee6cc209
DaveScott99 <davicalixto2077@gmail.com>
2024-12-31 09:49:37 -0300

    init project

---
+++ src/main/java/com/tiketeko/TikeTekoApplication.java
@@ -0,0 +1,13 @@
+package com.tiketeko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikeTekoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikeTekoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiketeko/TikeTekoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiketeko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikeTekoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
